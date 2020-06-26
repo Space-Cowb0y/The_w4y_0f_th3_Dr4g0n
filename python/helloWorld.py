1cea80e774a5d6214973c1b7a98b07be84ae15ee
Space_Cowb0y <brunohenrique.oliveiracarvalho@gmail.com>
2020-06-26 02:11:04 -0300

    New Python prog

---
+++ python/helloWorld.py
@@ -0,0 +1,24 @@
+# arquivo de aprendizagem 
+
+# imprimindo algo
+print ("hello World")
+
+# variaveis
+
+# podem ser declaradas como no java script
+# ou seja sem a didentificacao de tipo
+myint = 1
+myfloat = 1.10
+mystring = "hi" 
+
+# podemos fazer operacoes direto com as declaracoes
+print(myint+myint) # soma  1+1
+print(myint+myfloat) # soma 1 + 1.1
+
+# nao funciona se fizer com string print(myint + mystring)
+# deve se fazer da seguinte maneira
+
+print("%s:" % mystring + " " + "%d" % myint)
+
+# listas 
+
