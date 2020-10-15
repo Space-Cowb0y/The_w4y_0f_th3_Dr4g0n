f88a3633eacb82ebbb8db7f86ae012c6ad56a533
Space_Cowb0y <brunohenrique.oliveiracarvalho@gmail.com>
2020-10-15 14:44:57 -0300

    edit

--- python/Hello.py
+++ python/Hello.py
@@ -1,1 +1,9 @@
 print("hello, world")
+print("python é fácil")
+print("isto é uma string")
+
+idade = input('insira aqui usa idade: ')
+print(type(idade))
+idade = int(input('insira aqui usa idade: '))
+print(type(idade))
+
