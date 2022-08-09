95c576c2572c1202b5c16851d8a47160608f6c51
Space_Cowb0y <brunohenrique.oliveiracarvalho@gmail.com>
2022-08-08 21:20:08 -0300

    Working on WIP

---
+++ javaScript/termo_hack.js
@@ -0,0 +1,13 @@
+const solucao = JSON.parse(window.localStorage.termo).state;
+const solucaoduo = JSON.parse(window.localStorage.duo).state;
+const solucaoquatro = JSON.parse(window.localStorage.quatro).state;
+
+lista = [solucao, solucaoduo, solucaoquatro];
+
+for (x in lista) {
+    console.log(lista[x]);
+    for (y in lista[x]) {
+        resp = lista[x][y].solution;
+        console.log(resp);
+    }
+}
